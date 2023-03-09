6fec2560d1d20fe0b6612c3d3335821e29f5cb6b
EC2 Default User <ec2-user@ip-172-31-8-36.ap-northeast-1.compute.internal>
2023-03-09 08:45:26 +0000

    asynchronous

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,10 @@
 class FavoritesController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
     # if current_user==book.user
     #   redirect_to user_path(current_user)
     # else
@@ -13,10 +13,10 @@ class FavoritesController < ApplicationController
   end
   
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to books_path
+    # redirect_to books_path
   end
   
 end
